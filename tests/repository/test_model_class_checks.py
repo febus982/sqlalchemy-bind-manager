e6e7e715ee41cb645b6c847fa121f2a2e9a83b78
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-07-22 14:44:57 +0100

    Fail operations on models not belonging to repository

---
+++ tests/repository/test_model_class_checks.py
@@ -0,0 +1,23 @@
+import pytest
+
+from sqlalchemy_bind_manager.exceptions import InvalidModel
+
+
+async def test_fails_when_saving_models_not_belonging_to_repository(
+    repository_class, model_classes, sa_bind, sync_async_wrapper
+):
+    repo = repository_class(bind=sa_bind, model_class=model_classes[0])
+
+    invalid_model = model_classes[1](name="A Child")
+
+    with pytest.raises(InvalidModel):
+        await sync_async_wrapper(repo.save(invalid_model))
+
+    with pytest.raises(InvalidModel):
+        await sync_async_wrapper(repo.save_many([invalid_model]))
+
+    with pytest.raises(InvalidModel):
+        await sync_async_wrapper(repo.delete(invalid_model))
+
+    with pytest.raises(InvalidModel):
+        await sync_async_wrapper(repo.delete_many([invalid_model]))
