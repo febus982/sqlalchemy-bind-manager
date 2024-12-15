99fb008d231bdc798072bb320be02f6bc808635b
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-12-15 15:30:00 +0000

    Python 3.13 (#62)
    * Add support for python 3.13
    
    *Drop support for python 3.8
    
    * Bump up actions to python 3.13

--- tests/repository/result_presenters/test_composite_pk.py
+++ tests/repository/result_presenters/test_composite_pk.py
@@ -6,8 +6,11 @@ from sqlalchemy_bind_manager._repository.result_presenters import _pk_from_resul
 
 
 def test_exception_raised_if_multiple_primary_keys():
-    with patch(
-        "sqlalchemy_bind_manager._repository.result_presenters.inspect",
-        return_value=Mock(primary_key=["1", "2"]),
-    ), pytest.raises(NotImplementedError):
+    with (
+        patch(
+            "sqlalchemy_bind_manager._repository.result_presenters.inspect",
+            return_value=Mock(primary_key=["1", "2"]),
+        ),
+        pytest.raises(NotImplementedError),
+    ):
         _pk_from_result_object("irrelevant")
