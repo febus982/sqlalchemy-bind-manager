7bf9655f910c98b5089c8012703bcc1b09ab4a41
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-06-21 15:05:56 +0100

    Migrate to uv (#68)

--- scripts/docs-version.sh
+++ scripts/docs-version.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION=$(poetry version -s)
+# uv doesn't yet support reading dynamic version: https://github.com/astral-sh/uv/issues/14137
+#VERSION=$(uv version --short)
+VERSION=$(uv run scripts/version_from_git.py)
 SEMVER=( ${VERSION//./ } )
 echo "${SEMVER[0]}.${SEMVER[1]}"
